6d4ea1d010f9a6200dcefbc27448f33fc0d72e43
Francisco Alderete <franalderete7@gmail.com>
2023-05-26 02:01:34 -0300

    Initial Commit

---
+++ Resilience Pro/Resilience_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resilience_ProApp.swift
+//  Resilience Pro
+//
+//  Created by Francisco Alderete on 26/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Resilience_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
